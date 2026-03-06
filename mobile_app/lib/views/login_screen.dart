301599fe530c829d5fded04ef655205ab9d3ecc4
RobinLRN <robinlaurain@hotmail.fr>
2026-03-06 15:40:13 +0100

    Création de la page de connexion

--- mobile_app/lib/views/login_screen.dart
+++ mobile_app/lib/views/login_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
-import '../ui/widgets/widgets.dart';
 import '../ui/widgets/page_layout.dart';
+import '../ui/widgets/widgets.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>{
+class _LoginScreenState extends State<LoginScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final AuthService _authService = AuthService();
@@ -32,26 +32,87 @@ class _LoginScreenState extends State<LoginScreen>{
       _isLoading = false;
     });
 
-    if(sucess){
-      if(mounted){
-        ScaffoldMessenger.of( context).showSnackBar(
-          const SnackBar(content: Text('Connexion réussie!')),
-        );
+    if (sucess) {
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text('Connexion réussie!')));
       }
     } else {
       setState(() {
-        _errorMessage = 'Échec de la connexion. Veuillez vérifier vos identifiants.';
+        _errorMessage =
+            'Échec de la connexion. Veuillez vérifier vos identifiants.';
       });
     }
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
-    return PageLayout(
-      child: Column(
-        children: [
-          
-        ],
+    return Scaffold(
+      appBar: const CustomLogoAppBar(),
+      body: PageLayout(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Text(
+              'Se connecter à \n CESIZEN',
+              style: Theme.of(context).textTheme.displayLarge,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 50),
+            FormInput(
+              label: 'Email',
+              hint: 'Entrez votre adresse email',
+              controller: _emailController,
+              icon: Icons.email,
+            ),
+            const SizedBox(height: 50),
+            FormInput(
+              label: 'Mot de passe',
+              hint: 'Entrez votre mot de passe',
+              controller: _passwordController,
+              icon: Icons.lock,
+            ),
+            const SizedBox(height: 50),
+            MainButton(
+              text: 'Se connecter',
+              icon: Icons.arrow_forward, 
+              onPressed: _handleLogin,
+              ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Pas encore de compte ?',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print('Redirection vers la page d\'inscription');
+                  },
+                  child: Text(' S\'inscrire.',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: AppColors.tropicalTeal,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: (){
+                print('Redirection vers la page de mot de passe oublié');
+              },
+              child: Text('Mot de passe oublié ?',
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  color: AppColors.tropicalTeal,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ mobile_app/lib/views/register_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
+import '../ui/widgets/page_layout.dart';
+import '../ui/widgets/widgets.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
