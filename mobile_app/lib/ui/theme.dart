14b28f3ee6c4544ed8822f29bb9b6e1e292db444
RobinLRN <robinlaurain@hotmail.fr>
2026-03-06 13:54:41 +0100

    Modification du theme de l'app

--- mobile_app/lib/ui/theme.dart
+++ mobile_app/lib/ui/theme.dart
@@ -9,40 +9,114 @@ class AppColors {
   static const Color drySage = Color(0xFFB8B08D);
 }
 
+class AppTextStyles{
+  static TextStyle titleStyleH1 = GoogleFonts.merriweatherSans(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    color: AppColors.skyBlue,
+  );
 
-final ThemeData appTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: AppColors.darkCyan,
-    primary: AppColors.skyBlue,
-    secondary: AppColors.tropicalTeal,
-    surface: Colors.white,
-  ),
-  
-  textTheme: GoogleFonts.latoTextTheme().copyWith(
+  static TextStyle titleStyleH2 = GoogleFonts.merriweatherSans(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: AppColors.skyBlue,
+  );
+
+  static TextStyle titleStyleH3 = GoogleFonts.merriweatherSans(
+    fontSize: 21,
+    fontWeight: FontWeight.w700,
+    color: AppColors.skyBlue,
+  );
+
+  static TextStyle titleStyleH4 = GoogleFonts.merriweatherSans(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: AppColors.skyBlue,
+  );
+
+  static TextStyle titleStyleH5 = GoogleFonts.merriweatherSans(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: AppColors.skyBlue,
+  );
+
+  static TextStyle textStyleRegular = GoogleFonts.lato(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+    height: 1.5
+  );
+
+  static  TextStyle textStyleRegularBackground = GoogleFonts.lato(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.grey,
+    height: 1.5
+
+  );
+
+  static TextStyle textStyleBig = GoogleFonts.lato(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Colors.black
     
-    displayLarge: GoogleFonts.merriweatherSans(
-      fontSize: 32,
-      fontWeight: FontWeight.bold,
-      color: AppColors.darkCyan,
-    ),
-    displayMedium: GoogleFonts.merriweatherSans(
-      fontSize: 28,
-      fontWeight: FontWeight.bold,
-      color: AppColors.darkCyan,
-    ),
-    headlineMedium: GoogleFonts.merriweatherSans(
-      fontSize: 24,
-      fontWeight: FontWeight.w600,
-      color: AppColors.darkCyan,
-    ),
-    titleLarge: GoogleFonts.merriweatherSans(
-      fontSize: 20,
-      fontWeight: FontWeight.w600,
-      color: AppColors.darkCyan,
-    ),
-  ),
-);
+  );
+
+  static TextStyle textStyleBigBackground = GoogleFonts.lato(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Colors.grey
+  );
+
+
+  static  TextStyle textStyleLittle = GoogleFonts.lato(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+    height: 1.5
+    
+  );
+
+  static TextStyle textStyleLittleBackground = GoogleFonts.lato(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Colors.grey
+  );
+}
+
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: AppColors.tropicalTeal,
+      scaffoldBackgroundColor: Colors.white,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.tropicalTeal,
+        primary: AppColors.tropicalTeal,
+        secondary: AppColors.skyBlue,
+      ),
+      textTheme: TextTheme(
+        displayLarge: AppTextStyles.titleStyleH1,
+        displayMedium: AppTextStyles.titleStyleH2,
+        displaySmall: AppTextStyles.titleStyleH3,
+        headlineMedium: AppTextStyles.titleStyleH4,
+        headlineSmall: AppTextStyles.titleStyleH5,
+        bodyLarge: AppTextStyles.textStyleBig,
+        bodyMedium: AppTextStyles.textStyleRegular,
+        bodySmall: AppTextStyles.textStyleLittle,
+      ),
+      useMaterial3: true,
+    );
+  }
+}
+
+
+
+    // titleLarge: GoogleFonts.merriweatherSans(
+    //   fontSize: 20,
+    //   fontWeight: FontWeight.w600,
+    //   color: AppColors.darkCyan,
+ 
 
 
 //  titre
