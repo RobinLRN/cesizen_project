89124b3338ebaff3f1520b22e14f9a0e2b0257b1
Robin LRN <robinlaurain@hotmail.fr>
2026-03-06 19:01:34 +0100

    Création de la page d'accueil

---
+++ mobile_app/lib/views/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../ui/widgets/page_layout.dart';
+import '../ui/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomLogoAppBar(),
+      body: PageLayout(
+        child: Column(
+          children: [ 
+
+          ]
+        ),
+      )
+    );
+  }
+}
